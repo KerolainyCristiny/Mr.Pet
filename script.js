5806510b4a684e96b9d4e8b912a408ca253df18b
KerolainyCristiny <118860310+KerolainyCristiny@users.noreply.github.com>
2023-11-26 19:51:41 -0300

    Update HOME
    Atualização da Home, com novas funcionalidades e a adição do bootstrap

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 //Passar de forma automatica os slides da HOME
 
-let count = 1;
-document.getElementById("radio1").checked = true;
+// let count = 1;
+// document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-  nextImage();
-},10000)
+// setInterval( function(){
+//   nextImage();
+// },10000)
 
-function nextImage() {
-  count++;
-  if (count >4){
-    count = 1;
-  }
-  document.getElementById("radio"+count).checked = true;
+// function nextImage() {
+//   count++;
+//   if (count >4){
+//     count = 1;
+//   }
+//   document.getElementById("radio"+count).checked = true;
 
-}
+// }
 
 //fim dos slides
+
+
+//Favoritar
+
+// const img = document.querySelector(".favorito");
+// let favoritado = "./img/icone.svg"
+// let desfavorito = "./img/BotãoFavoritar.png"
+
+// function favoritar(){
+//   // const clicar = event.currentTarget;
+  
+//   if(img.src == favoritado){
+//     img.src = desfavorito;
+//   }else{
+//     img.src = favoritado
+//   }
+// }
+
+// if(img){
+// img.addEventListener("click", favoritar())
+// }
+
+//se for clicado, colocar no css display none e ficar trocando
